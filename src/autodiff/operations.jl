8f7f59d329c55eedcb1831efc19ba639e4f0b17e
AleksyBalazinski <aleksy.j.balazinski@gmail.com>
2024-05-29 16:09:25 +0200

    remove commented-out code

--- src/autodiff/operations.jl
+++ src/autodiff/operations.jl
@@ -15,17 +15,6 @@ import LinearAlgebra: mul!
 forward(::BroadcastedOperator{typeof(mul!)}, x, y) = return x * y
 backward(::BroadcastedOperator{typeof(mul!)}, x, y, g) = tuple(g * y', x' * g)
 
-# x .* y (element-wise multiplication)
-# Base.Broadcast.broadcasted(*, x::GraphNode, y::GraphNode) = BroadcastedOperator(*, x, y)
-# forward(::BroadcastedOperator{typeof(*)}, x, y) = return x .* y
-# backward(node::BroadcastedOperator{typeof(*)}, x, y, g) =
-#     let
-#         𝟏 = ones(length(node.output))
-#         Jx = diagm(y .* 𝟏)
-#         Jy = diagm(x .* 𝟏)
-#         tuple(Jx' * g, Jy' * g)
-#     end
-
 Base.Broadcast.broadcasted(-, x::GraphNode, y::GraphNode) = BroadcastedOperator(-, x, y)
 forward(::BroadcastedOperator{typeof(-)}, x, y) = return x .- y
 backward(::BroadcastedOperator{typeof(-)}, x, y, g) = tuple(g, -g)
@@ -33,36 +22,6 @@ backward(::BroadcastedOperator{typeof(-)}, x, y, g) = tuple(g, -g)
 Base.Broadcast.broadcasted(+, x::GraphNode, y::GraphNode) = BroadcastedOperator(+, x, y)
 forward(::BroadcastedOperator{typeof(+)}, x, y) = return x .+ y
 backward(::BroadcastedOperator{typeof(+)}, x, y, g) = tuple(g, g)
-
-# import Base: sum
-# sum(x::GraphNode) = BroadcastedOperator(sum, x)
-# forward(::BroadcastedOperator{typeof(sum)}, x) = return sum(x)
-# backward(::BroadcastedOperator{typeof(sum)}, x, g) =
-#     let
-#         𝟏 = ones(length(x))
-#         J = 𝟏'
-#         tuple(J' * g)
-#     end
-
-# Base.Broadcast.broadcasted(/, x::GraphNode, y::GraphNode) = BroadcastedOperator(/, x, y)
-# forward(::BroadcastedOperator{typeof(/)}, x, y) = return x ./ y
-# backward(node::BroadcastedOperator{typeof(/)}, x, y::Real, g) =
-#     let
-#         𝟏 = ones(length(node.output))
-#         Jx = diagm(𝟏 ./ y)
-#         Jy = (-x ./ y .^ 2)
-#         tuple(Jx' * g, Jy' * g)
-#     end
-
-# import Base: max
-# Base.Broadcast.broadcasted(max, x::GraphNode, y::GraphNode) = BroadcastedOperator(max, x, y)
-# forward(::BroadcastedOperator{typeof(max)}, x, y) = return max.(x, y)
-# backward(::BroadcastedOperator{typeof(max)}, x, y, g) =
-#     let
-#         Jx = diagm(isless.(y, x))
-#         Jy = diagm(isless.(x, y))
-#         tuple(Jx' * g, Jy' * g)
-#     end
 
 σ(x) = 1 / (1 + exp(-x))
 Base.Broadcast.broadcasted(σ, x::GraphNode) = BroadcastedOperator(σ, x)
